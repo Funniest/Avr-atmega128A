f762a48b67391208eec3ff256df76e242074169b
Funniest <ms.funniest@gmail.com>
2017-03-30 10:26:47 +0900

    Update 74LS373_test.c

--- 74LS373/74LS373_test.c
+++ 74LS373/74LS373_test.c
@@ -1,1 +1,28 @@
-t
+/*
+ * test.c
+ *
+ * Created: 2017-03-30 오전 9:38:04
+ * Author: Minsungkim
+ */
+
+#include <io.h>
+#include <delay.h>
+
+void main(void)
+{
+    DDRA = 0xFF; //DDRA DATA
+    DDRE = 0x04; //DDRP IS LE, OE
+    
+    //EX) LE = HIGH -> OE = LOW => NOT ACTIVE
+    //EX) LE = HIGH -> OE = HIGH => ACTIVE!
+    PORTE.0 = 1; //LE 1
+    PORTE.1 = 0; //OE 0
+    
+    while (1){
+    // Please write your application code here
+        PORTA.0 = 1;
+        delay_ms(1000);
+        PORTA.0 = 0;
+        delay_ms(3000);
+    }
+}
